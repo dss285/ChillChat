d4715b226cad7b66760d07f31e48e16e6039ed6a
cmdtvt <tvtuusa@gmail.com>
2022-11-15 18:30:24 +0200

    Added sass & scss to vue

--- ChillChat.Web/vue/src/App.vue
+++ ChillChat.Web/vue/src/App.vue
@@ -1,98 +1,44 @@
 <script setup lang="ts">
+//import "./assets/styles/style.sass";
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+//import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 
 
 
 <template>
-  <header>
-
-    
-    <div class="wrapper">
-     
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/chat">chat</RouterLink>
-        <RouterLink to="/signaldemo">SignalR demo</RouterLink>
-      </nav>
-    </div>
-  </header>
-  
-  <RouterView />
+	<header>
+		<div class="wrapper">
+			<nav>
+				<RouterLink to="/">Home</RouterLink>
+				<RouterLink to="/about">About</RouterLink>
+				<RouterLink to="/chat">chat</RouterLink>
+				<RouterLink to="/signaldemo">SignalR demo</RouterLink>
+			</nav>
+		</div>
+	</header>
+	<RouterView />
 </template>
 
 
-<style>
-  a {
-    margin-left: 20px;
-  }
-</style>
-
-<!--
-  <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+<style scoped lang="scss">
+  //@import "./assets/styles/style.sass";
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+	ul {
+		margin: 0;
+		padding: 0;
+		list-style: none;
+	}
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
+	li { display: inline-block; }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
+	a {
+		display: block;
+		padding: 6px 12px;
+		text-decoration: none;
+	}
+}  
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-  
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-  
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-  
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-    
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--->

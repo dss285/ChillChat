71aadf950466978efd385ec7f7ee2e401efbd217
dss285 <36992971+dss285@users.noreply.github.com>
2022-12-07 21:17:57 +0200

    refactor

---
+++ ChillChat.Vue/src/components/chat/ChannelComponent.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { Channel, Message } from '@/models/models';
+import MessageComponent from './MessageComponent.vue';
+
+
+const props = defineProps<{
+    channel : Channel
+}>()
+
+
+let messages : Message[] = []
+for(let i = 0; i<20;i++) {
+    let message = new Message();
+    message.Content = `${i} : Moi`
+    message.ChannelId = props.channel.ChannelId;
+    messages.push(message)
+}
+</script>
+
+<template>
+  <main>
+    <h2>{{channel.Name}}</h2>
+    <div>
+        <MessageComponent :message="message" v-for="message of messages"></MessageComponent>
+    </div>
+  </main>
+</template>
---
+++ ChillChat.Vue/src/components/chat/MessageComponent.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { Message } from '@/models/models';
+
+
+const props = defineProps<{
+    message : Message
+}>()
+</script>
+
+<template>
+  <main>
+    <div>
+        <h4>{{message.ChannelId}}</h4>
+        <p>{{message.Content}}</p>
+    </div>
+  </main>
+</template>

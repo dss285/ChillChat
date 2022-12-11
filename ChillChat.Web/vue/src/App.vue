949242f4fdf5db6d2a37422c008a0502c1ec34f3
cmdtvt <tvtuusa@gmail.com>
2022-12-11 22:03:04 +0200

    Frontpage design and some global styles.

--- ChillChat.Web/vue/src/App.vue
+++ ChillChat.Web/vue/src/App.vue
@@ -20,6 +20,7 @@ import Navbar from "./components/landing/Navbar.vue";
 
 <style lang="scss">
 
+@import url('https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap');
 
 :root {
     //Primary colors
@@ -40,15 +41,22 @@ import Navbar from "./components/landing/Navbar.vue";
 
     //Font settings
     --setting-font-size: 16px;
-    --setting-font-color: var(var(--color-quaternary));
+    --setting-font-color: white;
 
     //Padding & rounding
-    --setting-padding-primary: 10px;
-    --setting-padding-secondary: 5px;
+    --setting-padding-primary: 25px;
+    --setting-padding-secondary: 10px;
     --setting-Bradius-primary: 5px;
+    --setting-Bradius-secondary: 10px;
 
     //Misc
     --setting-scrollbehavior: smooth;
+    --setting-transition-primary: 0.25s;
+
+    //Margins
+    --margin-primary: 50px;
+    --margin-secondary: 25px;
+    --margin-tertiary: 15px;
 
 
 }
@@ -56,13 +64,52 @@ import Navbar from "./components/landing/Navbar.vue";
 html {
     scroll-behavior: var(--setting-scrollbehavior);
     font-size: var(--setting-font-size);
-
+    
 }
 
 .landing {
-
+    
     background-color: var(--color-primary);
+    background: rgb(20,53,238);
+    background: linear-gradient(140deg, rgba(20,53,238,1) 0%, rgba(12,18,54,1) 20%, rgba(0,0,0,1) 100%); 
+
+
     min-height: 100vh;
+    color: var(--setting-font-color);
+    font-family: 'Kanit', sans-serif;
+
+    .promobox {
+        margin-top: 20vh;
+        text-align: center;
+        font-size: 1.5em;
+        .title {
+            font-size: 3em;
+        }
+    }
+
+    
+    .services {
+        margin-top: 20vh;
+        .servicebox {
+            border: 4px solid var(--color-secondary);
+            border-radius: var(--setting-Bradius-secondary);
+            padding: var(--setting-padding-secondary);
+            transition: var(--setting-transition-primary);
+            cursor: pointer;
+
+            &:hover {
+                border: 4px solid var(--color-tertiary);
+                //box-shadow: 0px 0px 45px 10px rgba(238, 17, 164, 0.75);
+            }
+        }
+
+        .title {
+            text-align: center;
+            padding: var(--setting-Bradius-secondary);
+            margin-bottom: var(--margin-primary)
+        }
+    }
+    
 }
 
 </style>
--- ChillChat.Web/vue/src/views/LandingPageView.vue
+++ ChillChat.Web/vue/src/views/LandingPageView.vue
@@ -3,10 +3,87 @@ import TheWelcome from "../components/TheWelcome.vue";
 </script>
 
 <template>
-    <main class="landing">
+    <main class="container-fluid landing">
+
+        <div class="row">
+            <div class="col-md-12 promobox">
+                <h3 class="title">ChillChat</h3>
+                <p>Like a normal chat but bit more chill</p>
+                <a href="chat" class="btn btn-primary btn-full">Let's Go!</a>
+            </div>
+        </div>
 
 
+        <div class="row services">
 
-        <p>This is a landing page</p>
+
+            <div class="offset-md-1 col-md-10">
+                <div class="row">
+
+                    <div class="col-md-12 title">
+                        <h2>Services</h2>
+                    </div>
+        
+        
+                    <div class="col-md-4">
+                        <div class="offset-md-1 col-md-10 servicebox">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <p>WIP</p>
+                                    <hr>
+                                </div>
+            
+                                <div class="col-md-12">
+                                    <h4>TITLE</h4>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque dolorem laboriosam?</p>
+                                    <br>
+                                    <a href="home">Go</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+        
+                    <div class="col-md-4">
+                        <div class="offset-md-1 col-md-10 servicebox">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <p>WIP</p>
+                                    <hr>
+                                </div>
+            
+                                <div class="col-md-12">
+                                    <h4>TITLE</h4>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque dolorem laboriosam?</p>
+                                    <br>
+                                    <a href="home">Go</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+        
+        
+                    <div class="col-md-4">
+                        <div class="offset-md-1 col-md-10 servicebox">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <p>WIP</p>
+                                    <hr>
+                                </div>
+            
+                                <div class="col-md-12">
+                                    <h4>TITLE</h4>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque dolorem laboriosam?</p>
+                                    <br>
+                                    <a href="home">Go</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
     </main>
 </template>
